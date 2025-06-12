6b94e8f57b2d596d5e3d4430305a2d74d0f32121
cleribel00 <studycleribel@.com>
2025-06-12 04:06:44 -0400

    Programas del 4.1 al 4.21 del libro FUNDAMENTOS DE PROGRAMACION EN C

---
+++ Programas del 4.1 al 4.21/Programa 4.2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Cubo-2.
+El programa calcula el cubo de los 10 primeros números naturales con la
+ayuda de una función. */
+
+int cubo(void);
+int I;
+
+void main (void)
+{
+    int CUB;
+    for (I = 1; I <= 10; I++)
+    {
+        CUB = cubo();  /* Llamada a la función cubo. */
+         printf("\nEl cubo de %d es: %d", I, CUB);
+    }
+}
+int cubo(void)  /* Declaración de la función. */
+/* La función calcula el cubo de la variable local I. */
+{
+    int I = 2;  /* Variable local entera I con el mismo nombre
+que la variable global. */
+return (I*I*I);
+}
+
